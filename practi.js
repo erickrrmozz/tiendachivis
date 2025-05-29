bb73e82e0f7131ad27ee661e070a6322a13db24a
erickrrmozz <erickrrmoz@gmail.com>
2025-05-28 20:33:47 -0600

    Add files via upload

---
+++ practi.js
@@ -0,0 +1,11 @@
+     let nombre="erick";
+        console.log(nombre);
+
+        let apellido="rosales moz";
+        console.log(apellido);
+
+        let edad="17";
+        console.log(edad);
+
+        let mascotas="no";
+        console.log(mascotas);
